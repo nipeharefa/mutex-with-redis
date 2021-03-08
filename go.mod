64bc579e50d0d03bd94d5934281e94de6a36ae0c
Nipe Setiawan Harefa <nipeharefa@gmail.com>
2021-03-08 20:03:53 +0700

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module lemonilo-locking
+
+go 1.13
+
+require (
+	github.com/bsm/redislock v0.7.0
+	github.com/go-redis/redis/v8 v8.6.0
+)
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/bsm/redislock"
+	"github.com/go-redis/redis/v8"
+)
+
+// var ctx = context.Background()
+// u[dateCart with backoff options
+func updateCart(locker *redislock.Client, c chan int, ctx context.Context, i int) {
+
+	backoff := redislock.LinearBackoff(10 * time.Millisecond)
+	lock, err := locker.Obtain(ctx, "my-key", 2*time.Second, &redislock.Options{
+		RetryStrategy: backoff,
+	})
+	if err == redislock.ErrNotObtained {
+		fmt.Println("Could not obtain lock!", lock)
+	} else if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("lock saya", i)
+	lock.Release(ctx)
+	fmt.Println("release", i)
+	c <- 1
+}
+
+func sendUpdateCart(locker *redislock.Client) {
+	//
+	v := 10
+	ctx := context.Background()
+	jobs := make(chan int, v)
+
+	for i := 0; i < v; i++ {
+		go updateCart(locker, jobs, ctx, i)
+	}
+
+	for {
+		<-jobs
+		fmt.Println("next")
+	}
+
+}
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "10.126.0.88:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	defer rdb.Close()
+
+	// Create a new lock client.
+	locker := redislock.New(rdb)
+
+	sendUpdateCart(locker)
+
+}
